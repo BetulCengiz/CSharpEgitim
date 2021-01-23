f6f870a27a489587d404fa9fb8eacbe1ce0926b2
BetulCengiz <bc199911@hotmail.com>
2021-01-24 02:34:28 +0300

    02.34

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{   //prop>>>snippet
+    class Product
+    {
+        public int Id { get; set; }
+        // kod okunurluğu için sıralama önemlidir 
+        public int CategoryId { get; set; } 
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //CRUD operasyonları= silmek,eklemek,güncellemek gibi işlemlere denir
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //ProductManager = CRUD operasyonları içerir gibi anlayabilirsin
+
+        public void Add(Product product)
+        {
+            
+
+        }
+    }
+}
